9746ee3f8bd047e4c3930fd54b9e6269cca44b46
lwr27 <lewisrodhouse@gmail.com>
2026-06-05 08:47:02 +0100

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,79 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Variables
+variable "location" {
+  default = "East US"
+}
+
+variable "resource_group_name" {
+  default = "platform-lab-rg"
+}
+
+variable "acr_name" {
+  default = "platformlablwr27"
+}
+
+variable "aks_name" {
+  default = "platform-lab-aks"
+}
+
+# Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Azure Container Registry
+resource "azurerm_container_registry" "main" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+# AKS Cluster
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = "platformlab"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DC2as_v5"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+# Attach ACR to AKS
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.main.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.main.id
+  skip_service_principal_aad_check = true
+}
+
+# Outputs
+output "acr_login_server" {
+  value = azurerm_container_registry.main.login_server
+}
+
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.main.name
+}
