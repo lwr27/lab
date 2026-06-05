d7346c52ddaf265ad504dd29f0d6b2b2a18fa6b1
lwr27 <lewisrodhouse@gmail.com>
2026-06-05 10:03:22 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -79,10 +79,6 @@ output "aks_name" {
   value = azurerm_kubernetes_cluster.main.name
 }
 
-output "acr_login_server" {
-  value = azurerm_container_registry.main.login_server
-}
-
 output "acr_username" {
   value = azurerm_container_registry.main.admin_username
 }
@@ -92,6 +88,3 @@ output "acr_password" {
   sensitive = true
 }
 
-output "aks_name" {
-  value = azurerm_kubernetes_cluster.main.name
-}
